ae4290d5e19d16e11d3cbf426fae720fd6556fcd
Sabir7869 <sa15076332@gmail.com>
2025-08-03 03:45:59 +0530

    updatefiles

--- VolunteerHub/SpringBoot-web-Backend/src/main/java/com/webBackend/SpringBoot/web/Backend/Repo/ApplicantRepo.java
+++
@@ -1,9 +0,0 @@
-package com.webBackend.SpringBoot.web.Backend.Repo;
-
-import com.webBackend.SpringBoot.web.Backend.model.Applicant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApplicantRepo extends JpaRepository<Applicant,Long> {
-}
